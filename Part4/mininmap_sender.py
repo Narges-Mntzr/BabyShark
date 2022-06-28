0033bb8674ee70d2556e35881bb13b555b6a03e2
shirinbehnami <hhagh2002@gmail.com>
2022-06-28 22:53:15 +0430

    Test 4.

--- Part4/mininmap_sender.py
+++ Part4/mininmap_sender.py
@@ -4,10 +4,11 @@ from binascii import unhexlify, hexlify
 from checksum3 import cs
 import argparse
 
-#setup
+# setup
 fd = open('./info.txt', 'r')
 Lines = fd.readlines()
 interface0 = Lines[4].strip()
+
 
 def make_pkt(destination_port):
     dest_mac = Lines[6][:17]  # destination mac
@@ -15,7 +16,7 @@ def make_pkt(destination_port):
     proto3 = "08 00"  # layer 3 protocol number
     ver = "45"  # version, header lengh
     diff = "00"  # diffserv
-    t_len = "00 3c"  # total length ("00 28" for 40bytes, "00 3c" for 60 bytes)
+    t_len = "00 28"  # total length ("00 28" for 40bytes, "00 3c" for 60 bytes)
     id = "07 c3"  # id
     flags = "40 00"  # flags
     ttl = "40"  # ttl
@@ -23,7 +24,8 @@ def make_pkt(destination_port):
     cs3 = "00 00"  # ip check sum
 
     src_ip = hexlify(inet_aton(Lines[2])).decode()  # source ip
-    src_ip = src_ip[:2] + " " + src_ip[2:4] + " " + src_ip[4:6] + " " + src_ip[6:]
+    src_ip = src_ip[:2] + " " + src_ip[2:4] + \
+        " " + src_ip[4:6] + " " + src_ip[6:]
 
     dest_ip = hexlify(inet_aton(Lines[0])).decode()  # dest ip
     dest_ip = dest_ip[:2] + " " + dest_ip[2:4] + \
@@ -32,18 +34,16 @@ def make_pkt(destination_port):
     src_port = "%04x" % int(Lines[3])  # src port
     src_port = src_port[:2] + " " + src_port[2:]
 
-    dest_port = "%04x" % int(Lines[1])  # dest port
+    dest_port = "%04x" % int(destination_port)  # dest port
     dest_port = dest_port[:2] + " " + dest_port[2:]
 
     seq_num = "17 49 30 d1"  # seq number
     ack = "00 00 00 00"  # ack number
     # tcp header length and flags ("a0 02" for 40 bytes, "50 02" for 20 bytes)
-    h_len = "a0 02"
+    h_len = "50 02"
     w_size = "72 10"  # window size
     cs4 = "00 00"  # tcp check sum
     up = "00 00"  # urgent pointer
-
-    interface0 = Lines[4].strip()
 
     # ethernet
     ethernet_header = f'{dest_mac} {src_mac} {proto3}'
@@ -53,10 +53,7 @@ def make_pkt(destination_port):
     ip_header += f'{id} {flags} '
     ip_header += f'{ttl} {proto4} {cs3} '
     ip_header += f'{src_ip} '
-    ip_header += f'{dest_ip} '
-    ip_header += f'{src_port} {dest_port} '
-    ip_header += f'{seq_num} '
-    ip_header += f'{ack} '
+    ip_header += f'{dest_ip}'
 
     checksum = cs(ip_header)
     ip_header = ip_header[:30] + checksum[:2] + \
@@ -80,25 +77,22 @@ def make_pkt(destination_port):
     tcp_header = tcp_header[:47] + " " + checksum[0:2] + \
         " " + checksum[2:4]+" " + tcp_header[54:]
 
-
     packet = ethernet_header+" " + ip_header + " " + tcp_header
     packet = "".join(packet.split())
-    print(packet)
     return packet
 
 
 # send pkt
-def main(a,b):
-    for i in range(a,b):
+def main(a, b):
+    for i in range(a, b):
         pkt_bin = bin(int(make_pkt(i), 16))[2:]
         pkt_byte = bytes([int(i) for i in pkt_bin])
-
 
         try:
             s = socket(AF_PACKET, SOCK_RAW)
             s.bind((interface0, 0))
             # print(pkt_byte)
-            print(f'Sent {len(pkt_byte)}-byte packet on {interface0}')
+            print(f'Sent TCP SYN packet to port {i}')
         except:
             print(f'Something went wrong!!!')
 
@@ -114,5 +108,4 @@ if __name__ == '__main__':
     a = args.min
     b = args.max
 
-    main(a,b)
-
+    main(a, b)
